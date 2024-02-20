5f63c5396c8cf476035030bcba7cd7caee87fd00
Yashas-Vishwanath <122467316+Yashas-Vishwanath@users.noreply.github.com>
2024-02-20 16:55:28 +0100

    changes Lee said like to the setup bash file

--- turtlebot_robot/.docker/setup.bash
+++ turtlebot_robot/.docker/setup.bash
@@ -1,5 +1,5 @@
 source "/opt/ros/noetic/setup.bash"
 source "/dev_ws/devel/setup.bash"
 
-export ROS_MASTER_URI=http://10.41.1.1:11311
-export ROS_IP=10.41.1.1
+export ROS_MASTER_URI=http://192.168.11.79:11311
+export ROS_IP=192.168.11.79
