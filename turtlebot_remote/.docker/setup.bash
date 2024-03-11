ac8c361b7aa1761187032f4d769879c90ddbf221
Paco <122467316+Yashas-Vishwanath@users.noreply.github.com>
2024-03-11 16:44:49 +0100

    setup file for paco linux

--- turtlebot_remote/.docker/setup.bash
+++ turtlebot_remote/.docker/setup.bash
@@ -1,5 +1,10 @@
 source "/opt/ros/noetic/setup.bash"
 source "/dev_ws/devel/setup.bash"
 
+# my linux
 export ROS_MASTER_URI=http://192.168.11.79:11311
 export ROS_IP=192.168.11.190
+
+# paco linux
+export ROS_MASTER_URI=http://192.168.11.79:11311
+export ROS_IP=192.168.11.175
