1a89a5033ece011f449c6ec62b438b99c50a1a17
Yashas-Vishwanath <122467316+Yashas-Vishwanath@users.noreply.github.com>
2024-05-20 12:56:19 +0200

    arduino port test again

--- turtlebot_robot/.docker/entrypoint.sh
+++ turtlebot_robot/.docker/entrypoint.sh
@@ -4,6 +4,13 @@ set -e
 source "/opt/ros/noetic/setup.bash"
 source "/dev_ws/devel/setup.bash"
 
+# Set permissions for the serial port
+if [ -e /dev/ttyACM0 ]; then
+    chmod 666 /dev/ttyACM0
+else
+    echo "Device /dev/ttyACM0 not found. Make sure the Arduino is connected."
+fi
+
 export ROS_MASTER_URI=http://10.41.1.1:11311
 export ROS_IP=10.41.1.1
 exec "$@"
